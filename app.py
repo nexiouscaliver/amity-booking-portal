02359a91f944cc63eecdbf4f2ae5a8f7ab335054
Shahil kadia <66136088+nexiouscaliver@users.noreply.github.com>
2024-06-26 01:02:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 from time import ctime
 import os
+import random
 import dbscript as db
 import loginscript as login
 
@@ -51,6 +52,18 @@ def maketime(time:int):
         time=str(time)
         time = time[:2]+":"+time[2:]
         return time
+
+# global code_list
+code_list = []
+
+def gen_code():
+    # global code_list
+    code = random.randint(101,199999)
+    if code not in code_list:
+        code_list.append(code)
+    else:
+        code = gen_code()
+    return code
 
 def send_mail(event_name,event_venue,event_date,start_time,end_time,school_name,hoi_name,resource_person_name,resource_person_details,faculty_phone,faculty_name,faculty_email):
     email_body = f"""
@@ -110,6 +123,28 @@ def send_mail2(event_name,event_venue,event_date,start_time,end_time,school_name
     # Send the email
     try:
         msg = Message(subject='Response to Event Permission', recipients=[user_email])
+        msg.body = email_body
+        mail.send(msg)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def send_mail3(user_email,code):
+    email_body = f"""
+    Dear Faculty,
+
+    I hope this message finds you well.
+
+    Your Verification code for {user_email} is {code}.
+
+    Best regards,
+    AmiEventHub
+    """
+
+    # Send the email
+    try:
+        msg = Message(subject='Email Verification by AmiEventHub', recipients=[user_email])
         msg.body = email_body
         mail.send(msg)
         return True
@@ -222,15 +257,21 @@ def admin():
         facultyEmail = request.form["facultyEmail"]
         print(bid , status)
         if status=="ACCEPT":
-            db.confirm_hall(bid)
-            status2 = "ACCEPTED"
-            send_mail2(event_name,event_venue,event_date,start_time,end_time,school_name,resource_person_name,resource_person_details,status2,facultyEmail)
-            return redirect(url_for('admin'))
+            try:
+                db.confirm_hall(bid)
+                status2 = "ACCEPTED"
+                send_mail2(event_name,event_venue,event_date,start_time,end_time,school_name,resource_person_name,resource_person_details,status2,facultyEmail)
+                return redirect(url_for('admin'))
+            except:
+                return redirect(url_for('admin'))
         elif status=="REJECT":
-            db.reject_hall(bid)
-            status2 = "REJECTED"
-            send_mail2(event_name,event_venue,event_date,start_time,end_time,school_name,resource_person_name,resource_person_details,status2,facultyEmail)
-            return redirect(url_for('admin'))
+            try:
+                db.reject_hall(bid)
+                status2 = "REJECTED"
+                send_mail2(event_name,event_venue,event_date,start_time,end_time,school_name,resource_person_name,resource_person_details,status2,facultyEmail)
+                return redirect(url_for('admin'))
+            except:
+                return redirect(url_for('admin'))
     try:
         if session['username']:
             username = session['username']
@@ -267,12 +308,17 @@ def userlogin():
         username = request.form["user-username"]
         passwd = request.form["user-password"]
         #print(username,passwd)
-        if login.load_user(username,passwd):
+        if login.load_user(username,passwd) == True:
             session['username'] = username
             return redirect(url_for('index'))
         else:
-            error = "Incorrect Credentials!"
-            return render_template('loginuser.html',error=error)
+            if login.load_user(username,passwd) == "verify":
+                email = login.getname_user(username)
+                send_mail3(email,gen_code())
+                return redirect(url_for('verifyemail',username=username))
+            else:
+                error = "Incorrect Credentials!"
+                return render_template('loginuser.html',error=error)
     return render_template('loginuser.html',error=error)
 
 @app.route('/signup' ,methods=['GET', 'POST'])
@@ -284,14 +330,15 @@ def usersignup():
         passwd = request.form["user-password"]
         email = request.form["user-email"]
         
-        if (email.split("@")[-1]!="mum.amity.edu"):
+        if (email.split("@")[-1]!="s.amity.edu"):
             alert = 'Please use a valid @mum.amity.edu email address for signup.'
             return render_template('signup.user.html',error=alert)
         else:
             #return redirect(url_for('verifyemail',username=username,password=passwd))
             if login.create_user(username=username,password=passwd,name=email):
                 print(username,passwd,email)
-                return redirect(url_for('userlogin'))
+                send_mail3(email,gen_code())
+                return redirect(url_for('verifyemail',username=username))
             else:
                 alert = 'Account creation failed. Select different username and Please try again later.'
                 return render_template('signup.user.html',error=alert)
@@ -342,90 +389,38 @@ def adminsignup():
 
 #future
 
-
-
-@app.route('/sendemail', methods=['POST'])
-def sendemail():
-    event_data=[]
-    event_name = request.form['event_name']
-    event_venue = request.form['event_venue']
-    event_date = request.form['event_date']
-    start_time = request.form['start_time']
-    end_time = request.form['end_time']
-    school_name = request.form['school_name']
-    hoi_name = request.form['hoi_name']
-    resource_person_name = request.form['resource_person_name']
-    resource_person_details = request.form['resource_person_details']
-    faculty_phone = request.form['faculty_phone']
-    faculty_name = request.form['faculty_name']
-    faculty_email = request.form['faculty_email']
-    
-    # Create the email content
-    email_body = f"""
-    Dear Sir,
-
-    I hope this message finds you well.
-
-    I am writing to request your permission to conduct an event titled "{event_name}" at {event_venue} on {event_date} from {start_time} to {end_time}. Below are the details of the event:
-    1. School Name: {school_name}
-    2. Head of Institution/Director Name: {hoi_name}
-    3. Resource Person Name: {resource_person_name}
-    4. Resource Person Details: {resource_person_details}
-    5. Phone Number of Faculty: {faculty_phone}
-
-    We believe that this event will provide significant value and insights to our attendees, fostering an environment of learning and engagement.
-
-    For your convenience, please click on the link below to visit AmiEventHUB and grant us permission to organize this event:
-
-    [AmiEventHUB Link]
-
-    Best regards,
-    {faculty_name}
-    {school_name}
-    {faculty_email}
-    """
-
-    # Send the email
-    msg = Message('Request for Permission to Conduct an Event', recipients=[admin_email])
-    msg.body = email_body
-    mail.send(msg)
-    
-    # Save the event data for the admin page
-    event_data.append({
-        'event_name': event_name,
-        'event_venue': event_venue,
-        'event_date': event_date,
-        'start_time': start_time,
-        'end_time': end_time,
-        'school_name': school_name,
-        'hoi_name': hoi_name,
-        'resource_person_name': resource_person_name,
-        'resource_person_details': resource_person_details,
-        'faculty_phone': faculty_phone,
-        'faculty_name': faculty_name,
-        'faculty_email': faculty_email
-    })
-
-    return redirect(url_for('admin'))
-
-
-
-@app.route('/verifyemail/' ,methods=['GET', 'POST'])
-def verifyemail():
+@app.route('/verifyemail/<username>' ,methods=['GET', 'POST'])
+def verifyemail(username):
+    # global code_list
     alert = None
+    email = login.getname_user(username)
     if request.method == 'POST':
         code = request.form["verification-code"]
-        if (code == '123456'):
+        print(code_list)
+        print(code)
+        print(username)
+        if (int(code) in code_list):
             #alert = 'Please use a valid @mum.amity.edu email address for signup.'
-            print(code)
-            
-            return redirect(url_for('userlogin'))
+            print(code)    
+            state = login.verifyemail(username)
+            print(state)
+
+            if state == True :
+                return redirect(url_for('userlogin'))
+            else:
+                alert = "Incorrect server response entered please try again."
+                print(code)
+                print(code_list)
+                return render_template('verifyemail.html',error=alert,email=email)
         else:
             alert = "Incorrect code entered please try again."
             print(code)
-            return render_template('verifyemail.html',error=alert)
+            print(code_list)
+            return render_template('verifyemail.html',error=alert,email=email)
+    else:
+        return render_template('verifyemail.html',error=alert,email=email)
         
-    return render_template('verifyemail.html',error=alert)
+    return render_template('verifyemail.html',error=alert,email=email)
 
 #main runtime
 if __name__ == '__main__':
