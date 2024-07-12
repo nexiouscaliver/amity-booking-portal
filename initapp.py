8ab89a5504c129ecbf2cbb5dfe7f4af555b710da
nexiouscaliver <shahilkadia@gmail.com>
2024-07-13 02:47:01 +0530

    generate fix users on initialization

--- initapp.py
+++ initapp.py
@@ -8,4 +8,13 @@ login.init_db()
 for i in gu.final:
     u = i
     p = gu.final[i]
-    login.create_user(i,p)
+    print(i,p,i[:-6])
+    login.create_user(i,p,i[:-6])
+    print(f"created new user :{i} pass: {p} for school: {i[:-6]}")
+
+for i in gu.admin:
+    u = i
+    p = gu.admin[i]
+    print(i,p,i[:-6])
+    login.create_admin(i,p,i[:-6])
+    print(f"created new admin :{i} pass: {p} for name: {i[:-6]}")
