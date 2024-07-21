42519565bc8580049d21b4c2abbcce4e0a0298be
nexiouscaliver <shahilkadia@gmail.com>
2024-07-21 15:44:53 +0530

    add prod. server config and script

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from flask import jsonify, request, send_from_directory
 from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 
-import dbscript as db
-import loginscript as login
+import dbscript as db         #FOR SQL SERVER DATABASE
+import loginscript as login   #FOR SQL SERVER DATABASE
+
+#import dbscriptlite as db        #FOR SQLITE DATABASE
+#import loginscriptlite as login  #FOR SQLITE DATABASE
 
 app = Flask(__name__,
             static_url_path='', 
---
+++ linux-req.sh
@@ -0,0 +1,21 @@
+sudo apt update
+sudo apt install python3-pip python3-dev nginx -y
+sudo pip3 install virtualenv
+rm -rf AmityApp
+mkdir AmityApp && cd AmityApp
+virtualenv env
+source env/bin/activate
+pip3 install flask gunicorn
+pip3 -r install requirements.txt
+git clone https://github.com/nexiouscaliver/amity-booking-portal.git
+cd amity-booking-portal
+mv amiity-booking-portal/* .
+rm -rf amity-booking-portal
+gunicorn --bind 0.0.0.0:8000 wsgi:app
+# sudo cp amity-booking-portal.service /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl start amity-booking-portal
+# sudo systemctl enable amity-booking-portal
+# sudo cp amity-booking-portal /etc/nginx/sites-available/
+# sudo ln -s /etc/nginx/sites-available/amity-booking-portal /etc/nginx/sites-enabled
+# sudo systemctl restart nginx
