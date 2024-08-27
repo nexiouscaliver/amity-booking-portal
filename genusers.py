f21acb785b1ff8f9748c4947be0f963afbd46a9d
nexiouscaliver <shahilkadia@gmail.com>
2024-08-28 01:52:30 +0530

    update details

--- genusers.py
+++ genusers.py
@@ -44,7 +44,7 @@ def genpass():
 #     final[uname] = passwd
 # print(final)
 final = {'ASET_venue': 'venue_00686', 'ABS_venue': 'venue_29395', 'AFS_venue': 'venue_19823', 'AIBAS_venue': 'venue_96499', 'AIB_venue': 'venue_37460', 'AIIT_venue': 'venue_97950', 'AILA_venue': 'venue_22533', 'AIP_venue': 'venue_86056', 'AIT_venue': 'venue_65254', 'AITT_venue': 'venue_03396', 'ALS_venue': 'venue_39540', 'ASAP_venue': 'venue_31726', 'ASAS_venue': 'venue_58037', 'ASCO_venue': 'venue_56366', 'ASFT_venue': 'venue_28042', 'ASL_venue': 'venue_10844', 'CIIOL_venue': 'venue_06932', 'TUCSSBERICS_venue': 'venue_40988', 'ASFA_venue': 'venue_02634', 'AIE_venue': 'venue_81354', 'AIN_venue': 'venue_82477','Director_Admin_1': 'venue_22223','Director_Admin_2': 'venue_33334','CRC_Dept': 'venue_44445','HR_Dept': 'venue_55556','Hostel_Security_Dept': 'venue_66667','Admissions_Dept': 'venue_77778','Examinations_Dept': 'venue_88889','IT_dept':'venue_99990'}
-admin = {'Administrator': 'vcaum@123','Director_admin_1': 'venue_22222','Director_admin_2': 'venue_33333','Crc_dept': 'venue_44444','Hr_dept': 'venue_55555','Hostel_security_dept': 'venue_66666','Admissions_dept': 'venue_77777','Examinations_dept': 'venue_88888','IT_dept':'venue_99999','testdebug':'test'}
+admin = {'Administrator': 'vcaum@123','Director_admin_1': 'venue_dir1','Director_admin_2': 'venue_dir2','Crc_dept': 'venue_crc','Hr_dept': 'venue_hr','Hostel_security_dept': 'venue_hostel','Admissions_dept': 'venue_addm','Examinations_dept': 'venue_exam','IT_dept':'venue_it','testdebug':'test'}
 
 # f = open('example.txt','a')
 # for i in sc:
